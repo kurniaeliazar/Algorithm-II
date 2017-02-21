99ee688bfafa78317c737de92d1a783a0cad08bb
kurniaeliazar <kurniaeliazar@gmail.com>
2017-02-21 14:54:29 +0700

    commit day 3 material

---
+++ Day3/WithSeparator.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main (){
+
+ struct {
+ 	char maskapai[10]; 
+ 	int jumlah_penerbangan; 
+ 	char tujuan [50];
+ }flight;
+
+ FILE *Penerbangan;
+
+ int sum, sum_all;
+ char Separator[10] = "00";
+ char current [10];
+ Penerbangan = fopen("flight data separator.txt","rt");
+
+ sum_all = 0;
+ if (feof(Penerbangan)){
+  	printf("File Kosong,proses konsolidasi dibatalkan...!!!\n");
+ } else {
+  	fscanf (Penerbangan, "%s %d %s", flight.maskapai, &flight.jumlah_penerbangan, flight.tujuan);
+  	
+  	do {
+   		while(!feof(Penerbangan) && strcmp(flight.maskapai,Separator)==0) {
+     		fscanf (Penerbangan, "%s %d %s", flight.maskapai, &flight.jumlah_penerbangan, flight.tujuan);
+    	}
+
+   		sum = 0;
+   		strcpy(current, flight.maskapai);
+
+   		while(!feof(Penerbangan) && strcmp(flight.maskapai,Separator)!=0) {
+     		sum = sum + flight.jumlah_penerbangan;
+     		fscanf (Penerbangan, "%s %d %s", flight.maskapai, &flight.jumlah_penerbangan, flight.tujuan);
+    	}
+
+   		printf("jumlah penerbangan maskapai %s: %d\n", current, sum);
+   		sum_all = sum_all + sum;
+
+  }while(!feof(Penerbangan));
+
+  printf("Jumlah Seluruh Penerbangan : %d", sum_all);
+
+ }
+ 
+ fclose(Penerbangan);
+ return 0;
+}
---
+++ Day3/WithoutSeparator.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+  struct {
+    char maskapai[10]; 
+    int jumlah_penerbangan; 
+    char tujuan [50];
+  }flight;
+
+  FILE *PointerFile;
+  PointerFile = fopen("flight data.txt","rt");
+  fscanf (PointerFile, "%s %d %s", flight.maskapai, &flight.jumlah_penerbangan, flight.tujuan);
+  int sum, sum_all;
+  char current [10];
+
+  if (feof(PointerFile)){
+      printf("File Kosong,proses konsolidasi dibatalkan...!!!\n");
+  }
+  else{
+    //fscanf (PointerFile, "%s %d %s\n", flight.maskapai, &flight.jumlah_penerbangan, flight.tujuan);
+    sum_all = 0;
+    sum = 0;
+    strcpy(current, flight.maskapai);
+    do{
+
+      if(strcmp(flight.maskapai,current) == 0){
+        sum += flight.jumlah_penerbangan;
+      }else{
+        printf("Jumlah Penerbangan Maskapai %s: %d\n", current, sum);
+        sum = flight.jumlah_penerbangan;
+        strcpy(current, flight.maskapai);
+      }
+
+      sum_all = sum_all + flight.jumlah_penerbangan;
+      fscanf (PointerFile, "%s %d %s", flight.maskapai, &flight.jumlah_penerbangan, flight.tujuan);
+
+    }while (!feof(PointerFile));
+
+    printf("Jumlah Penerbangan Maskapai %s: %d\n", current, sum);
+    printf("Jumlah Seluruh Penerbangan : %d", sum_all);
+  }
+
+  fclose (PointerFile);
+  return 0;
+}
